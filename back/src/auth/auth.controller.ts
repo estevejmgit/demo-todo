e62926b35a9ff66ffed050553effaeb08ce8e25e
Jean-Marc <esteve.jm@gmail.com>
2025-02-18 06:47:41 +0100

    secure route ok

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +11,18 @@ export class AuthController {
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
+  }
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Get('protected')
+
+  @UseGuards(JwtAuthGuard)
+
+  getProtectedRoute(@Req() req) {
+    return { message: 'Accès autorisé', user: req.user };
   }
 }
